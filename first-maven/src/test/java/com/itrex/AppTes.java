d1fdef805edeae7146b5f569d9ade7f464a5185e
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-03-13 11:09:34 +0300

    Added default dependency chek App* in test faze

---
+++ first-maven/src/test/java/com/itrex/AppTes.java
@@ -0,0 +1,14 @@
+package com.itrex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTes { //не вызывается по умолчанию
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ first-maven/src/test/java/com/itrex/OlgaTest.java
@@ -0,0 +1,13 @@
+package com.itrex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class OlgaTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
