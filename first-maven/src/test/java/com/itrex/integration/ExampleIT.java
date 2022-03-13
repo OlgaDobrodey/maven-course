d57d5eab08dc8346bfe4dcb2f7d3607872232f8d
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-03-13 12:02:15 +0300

    added maven -failsafe-plugin

---
+++ first-maven/src/test/java/com/itrex/integration/ExampleIT.java
@@ -0,0 +1,12 @@
+package com.itrex.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleIT {
+
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
