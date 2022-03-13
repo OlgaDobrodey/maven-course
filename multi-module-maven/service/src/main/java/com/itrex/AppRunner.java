a0da5d4d3db395c81593c5517f093a0096ad6960
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-03-13 18:36:23 +0300

    resiaze

---
+++ multi-module-maven/service/src/main/java/com/itrex/AppRunner.java
@@ -0,0 +1,11 @@
+package com.itrex;
+
+import com.itrex.service.UserService;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+        var userService = new UserService();
+        System.out.println(userService.getUser(25L));
+    }
+}
