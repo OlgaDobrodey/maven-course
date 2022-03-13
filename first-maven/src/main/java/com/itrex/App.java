4381fad7728ab13908081c6493a391707a9ca641
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-03-13 15:47:16 +0300

    the finish version first-maven

---
+++ first-maven/src/main/java/com/itrex/App.java
@@ -0,0 +1,16 @@
+package com.itrex;
+
+import java.util.function.Supplier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Supplier<String> s =()-> "Hello World";
+        System.out.println( s.get() );
+    }
+}
---
+++ first-maven/src/test/java/com/itrex/AppTestCase.java
@@ -0,0 +1,14 @@
+package com.itrex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTestCase {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
